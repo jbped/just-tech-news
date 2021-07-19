dae277b0c0a7ff49378f715ca41855e3e3ab6b56
jbped <pedigojacob@gmail.com>
2021-07-19 08:42:30 -0600

    homepage template implemented and routed

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const apiRoutes = require("./api");
+const homeRoutes = require("./home-routes")
 
 router.use("/api", apiRoutes);
+
+router.use("/", homeRoutes);
 
 // Returns 404 err if a non-existent endpoint is attempted
 router.use((req,res) => {
